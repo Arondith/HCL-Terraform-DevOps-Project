5fc6b826a314076ae55c855f2a5f5e18a1ebdc6f
Arondite <Aronditee@gmail.com>
2026-09-19 13:47:28 +0800

    Add modules/network/outputs.tf

---
+++ modules/network/outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC."
+  value       = aws_vpc.this.id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets."
+  value       = [for subnet in aws_subnet.public : subnet.id]
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets."
+  value       = [for subnet in aws_subnet.private : subnet.id]
+}
+
+output "nat_gateway_enabled" {
+  description = "Whether the module created a NAT gateway."
+  value       = var.enable_nat_gateway
+}
